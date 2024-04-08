0732e075fac015b5493627556c04b924af9cf9d0
Everett Butler <everettjamesbutler@gmail.com>
2024-04-08 15:58:05 -0500

    got a main portfolio page created

---
+++ src/components/PortfolioMain/PortfolioMain.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function PortfolioMain() {
+
+return (
+<div>
+    
+</div>
+);
+
+}
+
+export default PortfolioMain;
