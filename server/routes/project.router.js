f63a236ca459289efec117343c16807af6b8a97f
Everett Butler <everettjamesbutler@gmail.com>
2024-04-08 23:55:57 -0500

    router should be set up now

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -5,8 +5,15 @@ const router = express.Router();
 // GET ROUTE
 router.get('/', (req, res) => {
     console.log('/project GET route');
+    let queryText = `SELECT * FROM "projects" ASC;`
+    pool.query(queryText).then((result) => {
+        console.log(result.rows)
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    })
 
-
-})
+});
 
 module.exports = router;
