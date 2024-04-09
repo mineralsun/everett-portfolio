589ada952c07f9488641c6dcd6300d905bf19ea6
Everett Butler <everettjamesbutler@gmail.com>
2024-04-08 23:31:02 -0500

    made project list reducer

---
+++ src/redux/reducers/projects.reducer.js
@@ -0,0 +1,10 @@
+const projectList = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_PROJECTS':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export default projectList;
