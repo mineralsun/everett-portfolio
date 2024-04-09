39d7e16c5b8794bef64eee911c8754467062b439
Everett Butler <everettjamesbutler@gmail.com>
2024-04-08 23:22:22 -0500

    started setting up redux

--- src/components/PortfolioMain/PortfolioMain.jsx
+++ src/components/PortfolioMain/PortfolioMain.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './PortfolioMain.css';
 
 function PortfolioMain() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const projects = useSelector(store => store.projectList);
 
 return (
-<div>
+<main>
+<div className="container">
 <h1>Welcome to your new developer's portfolio!</h1>
 <h5>Everett is the best coder!</h5>
 </div>
+</main>
 );
 
 }
