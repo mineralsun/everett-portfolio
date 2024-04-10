a2d45f20478648f8789205b2cf203ff5a4c90dc9
Everett Butler <everettjamesbutler@gmail.com>
2024-04-10 15:33:41 -0500

    found a theme I liked and applied it to project

--- src/components/PortfolioMain/PortfolioMain.jsx
+++ src/components/PortfolioMain/PortfolioMain.jsx
@@ -13,24 +13,27 @@ function PortfolioMain() {
     }, []);
 
     return (
-        <main>
+        <main className="mainPage">
             <h1>Welcome to your new developer's portfolio!</h1>
             <h5>Everett is the best coder!</h5>
+            <div className="mainContainer">
+                <h2>My Past Projects!</h2>
+            </div>
             <section className="projectGrid">
                 {projects.map(project => {
                     return (
-                                <div className="projectContainer" key={project.id}>
-                                    <img src={project.thumbnail} width={320} height={180} />
-                                    <div className="projectInfo">
-                                    <h2>{project.project_name}</h2>
-                                    <p id="descriptionStyle">{project.project_description}</p>
-                                    <div className="sourceAndDemo">
+                        <div className="projectContainer" key={project.id}>
+                            <img src={project.thumbnail} width={320} height={180} />
+                            <div className="projectInfo">
+                                <h2>{project.project_name}</h2>
+                                <p id="descriptionStyle">{project.project_description}</p>
+                                <div className="sourceAndDemo">
                                     <a href={project.live_demo} target="_blank" rel="noopener noreferrer">Live Demo</a>
                                     <br />
                                     <a href={project.source_code} target="_blank" rel="noopener noreferrer">Source Code</a>
-                                    </div>
-                                    </div>
                                 </div>
+                            </div>
+                        </div>
                     )
                 })}
             </section>
