aeef8866bca723b4723b56a3fd5e89d78db4815b
Everett Butler <everettjamesbutler@gmail.com>
2024-04-10 16:38:37 -0500

    styling more on the about me section!

--- src/components/PortfolioMain/PortfolioMain.jsx
+++ src/components/PortfolioMain/PortfolioMain.jsx
@@ -13,9 +13,40 @@ function PortfolioMain() {
     }, []);
 
     return (
-        <main className="mainPage">
-            <h1>Welcome to your new developer's portfolio!</h1>
-            <h5>Everett is the best coder!</h5>
+        <main>
+            <section className="welcomePage">
+                <h1>Welcome to your new developer's portfolio!</h1>
+            </section>
+            <section className="aboutMeContainer">
+                <div className="aboutMe">
+                    <h2>About Me</h2>
+                    <p>I am currently <b>available for hire!</b></p>
+                    <p>Apart from work, I am a<i>passionate musician</i>, I produce my own music, play guitar, and support local musicians wherever I am!</p>
+                    <p>I love <i>collaborating</i> with other driven and artistic people who want to do work that <i>matters</i> and makes a difference in this world!</p>
+                    <hr />
+                    <h2>My Focus</h2>
+                    <p>A full-stack engineer with a preference in back-end development!</p>
+                    <a target="_blank" href="public/files/Everett Butler Master Resume 2024.pdf">My Resume</a>
+                    <div className="techStack">
+                        <ul>
+                        <li>HTML/CSS</li>
+                        <li>JavaScript</li>
+                        <li>SQL</li>
+                        <li>C#</li>
+                        <li>.NET</li>
+                        <li>Python</li>
+                        </ul>
+                        <ul>
+                        <li>React/Redux</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>PostgreSQL</li>
+                        <li>AWS/S3 Bucket</li>
+                        <li>Postman</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
             <div className="mainContainer">
                 <h2>My Past Projects!</h2>
             </div>
